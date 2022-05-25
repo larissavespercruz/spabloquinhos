b7238cf7b549c3cae629292131d4666e3d23a2ea
Larissa Ruster Vesper Cruz <lrcruz@grupogbi.com>
2022-05-25 20:48:21 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home/home.component';
+import { ProductComponent } from './home/home/product/product.component';
+import { AboutComponent } from './home/home/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
